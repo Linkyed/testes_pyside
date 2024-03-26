4c2bee7c5e58eb4ac5b18016d65fa63b2ede4bcd
Pedro Henrique A <phaalmeida1@gmail.com>
2024-03-26 20:31:00 -0300

    Usando classe para instanciar uma janela inteira

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
 from PySide6.QtWidgets import QApplication, QLabel, QPushButton, QWidget, QVBoxLayout, QMainWindow
 from PySide6.QtGui import QFont, QAction
 from PySide6.QtCore import Qt
+from qdarktheme import load_stylesheet
+
+
+
+class Window (QMainWindow):
+    def __init__(self):
+        super().__init__()
+        base = QWidget()
+        layout = QVBoxLayout()
+
+        menu = self.menuBar()
+        arquivo_menu = menu.addMenu('Arquivo')
+        action = QAction('Print!')
+        arquivo_menu.addAction(action)
+
+        font = QFont()
+        font.setPixelSize(50)
+        font.setItalic(True)
+
+        self.label = QLabel('0')
+        self.label.setFont(font)
+        self.label.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.label)
+
+        botao = QPushButton('Aumentar numero')
+        botao.setFont(font)
+        botao.clicked.connect(self.printTerminal)
+        layout.addWidget(botao)
+
+        base.setLayout(layout)
+        self.setCentralWidget(base)
+        self.setWindowTitle('Application')
+
+    def printTerminal(self):
+        numeroAtual = self.label.text()
+        if numeroAtual.isnumeric() == True:
+            numeroAtual = int(numeroAtual)
+            numeroAtual += 1
+        else:
+            numeroAtual = 'Erro!'
+        
+        self.label.setText(str(numeroAtual))
+
 
 app = QApplication()
-mainWindow = QMainWindow()
-base = QWidget()
-layout = QVBoxLayout()
-
-menu = mainWindow.menuBar()
-arquivo_menu = menu.addMenu('Arquivo')
-action = QAction('Print!')
-arquivo_menu.addAction(action)
-
-font = QFont()
-font.setPixelSize(50)
-font.setItalic(True)
-
-label = QLabel('Olá Mundo')
-label.setFont(font)
-label.setAlignment(Qt.AlignCenter)
-layout.addWidget(label)
-
-botao = QPushButton('Aperte Aqui!')
-botao.setFont(font)
-layout.addWidget(botao)
-
-base.setLayout(layout)
-
-mainWindow.setCentralWidget(base)
-mainWindow.setWindowTitle('Application')
+app.setStyleSheet(load_stylesheet())
+mainWindow = Window()
 mainWindow.show()
 
 app.exec()

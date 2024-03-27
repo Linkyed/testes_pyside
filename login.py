b499fa3629e5cd73dd4a38a28e9608e9fbc89922
Pedro Henrique A <phaalmeida1@gmail.com>
2024-03-26 21:38:58 -0300

    Testando usar XML do Qt Design

---
+++ login.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+
+app = QApplication()
+
+loader = QUiLoader()
+window = loader.load('login.ui')
+window.show()
+
+app.exec()
